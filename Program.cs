922c19d9a96d8f00d1b0899a1815b8a14097580b
Ana Maria <anamaria27988@gmail.com>
2023-03-30 22:31:33 +0300

    new

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+            TwoSum(input,target);
+            int [] TwoSum(int[] nums, int target)
+            {
+                int[] newN = new int[2];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    int currNum = nums[i];
+                    for (int j = i+1; j < nums.Length; j++)
+                    {
+                        if (currNum + nums[j] == target)
+                        {
+                            string [] output = {$"{i}",$"{j}"};
+                            int[] numbers = { i,j };
+                            return numbers;
+                        }
+                    }
+                }
+                return newN;
+            }
+            
+        }
+    }
+}
